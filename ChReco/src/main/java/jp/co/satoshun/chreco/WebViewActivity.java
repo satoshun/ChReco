f8857f198834463f0481f529f4d401ece24f7c45
Sato Shun <shun.sato1@gmail.com>
2013-07-14 17:03:53 +0900

    webview

---
+++ ChReco/src/main/java/jp/co/satoshun/chreco/WebViewActivity.java
@@ -0,0 +1,22 @@
+package jp.co.satoshun.chreco;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.LinearLayout;
+import jp.co.satoshun.chreco.libs.MyWebView;
+
+public class WebViewActivity extends Activity {
+    LinearLayout layout;
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+
+        layout = (LinearLayout) findViewById(R.id.main_webview_layout);
+        webView = new MyWebView(getApplicationContext());
+        layout.addView(webView);
+    }
+}
---
+++ ChReco/src/main/java/jp/co/satoshun/chreco/libs/MyWebView.java
@@ -0,0 +1,26 @@
+package jp.co.satoshun.chreco.libs;
+
+
+import android.content.Context;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MyWebView extends WebView {
+
+    public MyWebView(Context context) {
+        super(context);
+        init();
+    }
+
+    public void init() {
+        WebSettings settings = getSettings();
+        settings.setLoadWithOverviewMode(true);
+        settings.setUseWideViewPort(true);
+        requestFocus(View.FOCUS_DOWN);
+
+        setWebViewClient(new WebViewClient() {
+        });
+    }
+}
